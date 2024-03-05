6e88a87d47c93ff160d5c38e35e8c36226905442
Aleksandr <63162751+Galxx@users.noreply.github.com>
2024-03-05 18:24:50 +0900

    init

---
+++ src/main/java/com/example/testbase2/TestBase2Application.java
@@ -0,0 +1,13 @@
+package com.example.testbase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBase2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBase2Application.class, args);
+    }
+
+}
