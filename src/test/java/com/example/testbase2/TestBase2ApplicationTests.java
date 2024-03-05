a6ce00c4b32087fd1005ec0aad399b15d439ba18
Aleksandr <63162751+Galxx@users.noreply.github.com>
2024-03-05 18:25:00 +0900

    init 2

---
+++ src/test/java/com/example/testbase2/TestBase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testbase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBase2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
